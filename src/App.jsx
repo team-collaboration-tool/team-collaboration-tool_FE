df18c13e881c3b997d0bbe4b2963725f3b2d5ebe
KIM GAYOUNG <165864773+kayeong97@users.noreply.github.com>
2025-12-06 17:47:54 +0900

    fix: App.jsx 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
 
 // pages/auth
 import Login from "./pages/auth/Login";
@@ -13,7 +19,6 @@ import Setting from "./pages/main/Setting";
 import Layout from "./Layout";
 
 // pages/projects/project
-import Project from "./pages/projects/project/Project";
 import ProjectSetting from "./pages/projects/project/ProjectSetting";
 
 // pages/projects/calendar
@@ -21,13 +26,9 @@ import Calendar from "./pages/projects/calendar/Calendar";
 
 // pages/projects/board
 import Board from "./pages/projects/board/Board";
-import BoardDetail from "./pages/projects/board/BoardDetail";
-import AddBoard from "./pages/projects/board/AddBoard";
 
 // pages/projects/schedule
 import Schedule from "./pages/projects/schedule/Schedule";
-import AddSchedule from "./pages/projects/schedule/AddSchedule";
-import ScheduleDetail from "./pages/projects/schedule/ScheduleDetail";
 
 // pages/NotFound
 import NotFound from "./pages/NotFound";
@@ -54,36 +55,37 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 인증 관련 라우트 */}
-        <Route path="/" element={<Login />} />
-        <Route path="/sign/1" element={<SignUp />} />
-
-        {/* 메인 대시보드 */}
-        <Route element={<Layout showPageNav={false} />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/setting" element={<Setting />} />
+        <Route element={<PublicRoute />}>
+          {/* 인증 관련 라우트 */}
+          <Route path="/" element={<Login />} />
+          <Route path="/sign/1" element={<SignUp />} />
         </Route>
 
-        {/* 프로젝트 라우트 */}
-        <Route
-          path="/project/:projectID"
-          element={<Layout showPageNav={true} />}
-        >
-          <Route path="calendar" index element={<Calendar />} />
-          <Route path="projectsetting" element={<ProjectSetting />} />
-
-          {/* 게시판 라우트 */}
-          <Route path="board">
-            <Route index element={<Board />} />
-            <Route path="create" element={<AddBoard />} />
-            <Route path=":PostID" element={<BoardDetail />} />
+        <Route element={<PrivateRoute />}>
+          {/* 메인 대시보드 */}
+          <Route element={<Layout showPageNav={false} />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/setting" element={<Setting />} />
           </Route>
 
-          {/* 일정 조율 라우트 */}
-          <Route path="schedule">
-            <Route index element={<Schedule />} />
-            <Route path="add" element={<AddSchedule />} />
-            <Route path=":scheduleID" element={<ScheduleDetail />} />
+          {/* 프로젝트 라우트 */}
+          <Route
+            path="/project/:projectID"
+            element={<Layout showPageNav={true} />}
+          >
+            <Route index element={<Calendar />} />
+            <Route path="projectsetting" element={<ProjectSetting />} />
+            <Route path="calendar" element={<Calendar />} />
+
+            {/* 게시판 라우트 */}
+            <Route path="board">
+              <Route index element={<Board />} />
+            </Route>
+
+            {/* 일정 조율 라우트 */}
+            <Route path="schedule">
+              <Route index element={<Schedule />} />
+            </Route>
           </Route>
         </Route>
 
